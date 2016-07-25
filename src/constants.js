aba9a60a57f131113520ba6286ce3d0db36c6625
Angelo Ashmore <angeloashmore@me.com>
2016-07-24 19:55:02 -1000

    Fix bug with folding
    The RegExp was too general and broke when the property's value contained line
    breaks.

--- src/constants.js
+++ src/constants.js
@@ -4,7 +4,7 @@ export const PARAMETER_SEPARATOR = ';'
 export const PARAMETER_KV_SEPARATOR = '='
 export const PROPERTY_KV_SEPARATOR = ':'
 
-export const FOLD_REGEX = new RegExp(/.{1,75}/g)
+export const FOLD_REGEX = new RegExp(/[\s\S]{1,75}/g)
 
 export const PREFIX = 'BEGIN'
 export const SUFFIX = 'END'
